bd11e66625656693e884546d51f26de301f080ec
Jordan Loop and Jun Fritz <jordan+jun@>
2017-07-03 10:38:40 -0700

    add separate task class.

--- app/app.component.ts
+++ app/app.component.ts
@@ -24,8 +24,10 @@ export class AppComponent {
   month: number = this.currentTime.getMonth() +1;
   day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear();
+  firstTask: Task = new Task("Finish weekend Angular homework for Epicodus course");
+}
 
-  firstTask = {
-    description: "Finish weekend Angular homework for Epicodus course"
-  }
+export class Task {
+  public done: boolean = false;
+  constructor(public description: string){}
 }
