579037e479798d1130ebf070ca450abf8e6b9182
Jordan Loop and Jun Fritz <jordan+jun@>
2017-07-03 10:30:55 -0700

    add properties to AppComponent class in 'component.ts'

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,12 +6,26 @@ import { Component } from '@angular/core';  //provides app access to angular com
   selector: 'app-root', //looks for <app-root> tag in index.html
   template: `
   <div class="container">
-    <h1>My First Angular 2 App</h1>
+    <h1>{{appTitle}} for {{month}}/{{day}}/{{year}}</h1>
+    <h3>{{currentFocus}}</h3>
+    <ul>
+      <li>{{firstTask.description}}</li>
+    </ul>
   </div>
   `
 })
 //Component Annotation End
 
 export class AppComponent {
+  appTitle: string = 'To Do List';
+  currentFocus: string = 'Angular Homework';
 
+  currentTime = new Date();//if you want to reference variable within the class you must use "this."
+  month: number = this.currentTime.getMonth() +1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
+
+  firstTask = {
+    description: "Finish weekend Angular homework for Epicodus course"
+  }
 }
